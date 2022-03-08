803fafd1ae5b660d9a2fa40e482d7f5e1d0f57f8
vfrydrychowski <frydrychowskiv@gmail.com>
2022-03-08 14:26:00 +0100

    distance calculation

--- analyse.py
+++ analyse.py
@@ -7,3 +7,10 @@ import matplotlib.pyplot as plt
 #out: nouveau dataframe avec insertion de la vitesse
 def calcVitesse(df):
     return df.assign('Vitesse', np.sign(df.get('[00].VehicleUpdate-speed.001'))*np.sqrt(df.get('[00].VehicleUpdate-speed.001')**2 + df.get('[00].VehicleUpdate-speed.002')**2 + df.get('[00].VehicleUpdate-speed.003')**2))
+
+#calcule la distance parcourue de la voiture du début du dataframe à la fin
+#Colonne panda 'Vitesse' doit être présente
+#in: dframe
+#out: nouveau dataframe avec insertion de la distance
+def calcDistance(df):
+    return df.assign(0, 'distance', (df.get('Vitesse')*df.index[1]).cumsum())
