07a2b9f8950a50d6525b0dfdb9deea1ef9adfc1e
vfrydrychowski <frydrychowskiv@gmail.com>
2022-04-11 08:57:51 +0200

    acceleration mse score contribution

--- analyse.py
+++ analyse.py
@@ -119,12 +119,17 @@ def createClosestD(df1,df2):
 def vitesseMSE(df1,df2):
     return ((df1['Vitesse'].to_numpy() - df2['Vitesse'].to_numpy())**2).mean()
 
+#fonction de calcul de proximité basique des courbes d'accceleration
+#in deux dataframes indexés sur 'distance'. Doivent contenir le champs 'Acceleration'
+def accelerationMSE(df1,df2):
+    return ((df1['Acceleration'].to_numpy() - df2['Acceleration'].to_numpy())**2).mean()
+
 #renvoi le score de proximité de df avec df1 et df2
 #out : si < 0 , le style df1 est le plus ressemblant, si > 0 c'est le stle df2
 def score(df,df1,df2):
     ndf1 = createClosestD(df,df1)
     ndf2 = createClosestD(df, df2)
-    return vitesseMSE(df,ndf1) - vitesseMSE(df,ndf2)
+    return vitesseMSE(df,ndf1) - vitesseMSE(df,ndf2) + accelerationMSE(df,ndf1) - accelerationMSE(df,ndf2)
 
 #renvoie les scores des vitesses poour chaques tronçons 
 def get_score(dfa, df1, df2):
